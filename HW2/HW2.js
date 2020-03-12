f1fbfdbfced8fcf350852f95bcdb1ced4a030934
Alexander Eryomenko <47386083+Alexander-Eryomenko@users.noreply.github.com>
2020-03-12 16:15:05 +0200

    Add files via upload

---
+++ HW2/HW2.js
@@ -0,0 +1,22 @@
+var letters = []
+var str = "Backend As A Service"
+var arrStr = str.split(' ')
+for ( var i = 0; i < arrStr.length; i++ ){
+    var firstLetter = arrStr[i].charAt(0) + ','
+    console.log (firstLetter)
+}
+
+
+function tip (type) {
+
+    if (typeof type === 'number'){
+       return console.log (new Date ())
+    }
+    else {
+        console.log ("Неверный тип данных")
+    }
+}
+
+tip (5)
+tip ('5')
+
